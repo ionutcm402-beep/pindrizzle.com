c57b3b791e6917291c82dd8d33c5447d0dd48225
IonutCM <ionutcm402@gmail.com>
2026-08-26 22:20:35 +0100

    Design system: load internal operations layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ import "./ping-you-system.css";
 import "./ping-business-system.css";
 import "./ping-onboarding-system.css";
 import "./ping-utility-system.css";
+import "./ping-internal-system.css";
 
 export const metadata: Metadata = {
   title: "Ping — Know what's happening in your mile",
