0286f6f762c290d5c993c3a3e971f6b5e143fbcf
IonutCM <ionutcm402@gmail.com>
2026-08-27 00:06:10 +0100

    Load My Pings design system styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ import "./ping-map-system.css";
 import "./ping-search-system.css";
 import "./ping-alerts-system.css";
 import "./ping-you-system.css";
+import "./ping-my-pings-system.css";
 import "./ping-business-system.css";
 import "./ping-onboarding-system.css";
 import "./ping-utility-system.css";
