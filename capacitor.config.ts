8684da0d7997d80ef4e4e011b45985b05ffd4339
IonutCM <ionutcm402@gmail.com>
2026-08-28 03:31:51 +0100

    ios: use light status content on navy

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -28,7 +28,7 @@ const config: CapacitorConfig = {
     },
     StatusBar: {
       overlaysWebView: true,
-      style: "LIGHT",
+      style: "DARK",
       backgroundColor: "#082b49",
     },
   },
