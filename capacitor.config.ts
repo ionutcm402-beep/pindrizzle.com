6c1b646e6aef1a297177346276b0acc6a18c09f9
IonutCM <ionutcm402@gmail.com>
2026-08-28 02:53:12 +0100

    feat: configure Pindrizzle Capacitor app

---
+++ capacitor.config.ts
@@ -0,0 +1,37 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.pindrizzle.app",
+  appName: "Pindrizzle",
+  webDir: "out",
+  backgroundColor: "#eef5f7",
+  server: {
+    androidScheme: "https",
+    cleartext: false,
+  },
+  ios: {
+    contentInset: "never",
+    scrollEnabled: false,
+  },
+  android: {
+    backgroundColor: "#eef5f7",
+  },
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true,
+      launchShowDuration: 500,
+      backgroundColor: "#eef5f7",
+      androidScaleType: "CENTER_INSIDE",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+    StatusBar: {
+      overlaysWebView: true,
+      style: "DARK",
+      backgroundColor: "#eef5f7",
+    },
+  },
+};
+
+export default config;
