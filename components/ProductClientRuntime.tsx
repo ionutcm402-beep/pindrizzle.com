3e80ebda8c5cc8223a58b92aa29331fc824505b9
IonutCM <ionutcm402@gmail.com>
2026-08-28 18:46:40 +0100

    Reduce idle client runtime work

--- components/ProductClientRuntime.tsx
+++ components/ProductClientRuntime.tsx
@@ -1,40 +1,59 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import PasswordAuthOverlay from "@/components/PasswordAuthOverlay";
 import Phase5PingDetail from "@/components/Phase5PingDetail";
 import PrivacySafetyCenter from "@/components/PrivacySafetyCenter";
 import Phase6NotificationBadge from "@/components/Phase6NotificationBadge";
-import Phase7VisibilityBridge from "@/components/Phase7VisibilityBridge";
-import Phase7ContributorContext from "@/components/Phase7ContributorContext";
-import Phase8FollowBridge from "@/components/Phase8FollowBridge";
 import Phase16PushSafetyBridge from "@/components/Phase16PushSafetyBridge";
 import Phase19ProductAnalytics from "@/components/Phase19ProductAnalytics";
 import Phase21AccessibilityBridge from "@/components/Phase21AccessibilityBridge";
 import Phase22StorageChoice from "@/components/Phase22StorageChoice";
 import Phase24BetaBridge from "@/components/Phase24BetaBridge";
-import Phase25LocationChoiceBridge from "@/components/Phase25LocationChoiceBridge";
 import PindrizzleCopyBridge from "@/components/PindrizzleCopyBridge";
 
 const FirstRunOnboarding = dynamic(() => import("@/components/FirstRunOnboarding"));
+const Phase7VisibilityBridge = dynamic(() => import("@/components/Phase7VisibilityBridge"));
+const Phase7ContributorContext = dynamic(() => import("@/components/Phase7ContributorContext"));
+const Phase8FollowBridge = dynamic(() => import("@/components/Phase8FollowBridge"));
 const Phase8SinceLastVisit = dynamic(() => import("@/components/Phase8SinceLastVisit"));
 const Phase8NearbyPulse = dynamic(() => import("@/components/Phase8NearbyPulse"));
 const Phase9PromotedLocal = dynamic(() => import("@/components/Phase9PromotedLocal"));
 const Phase14SearchEntry = dynamic(() => import("@/components/Phase14SearchEntry"));
 const Phase15PlaceIntelligence = dynamic(() => import("@/components/Phase15PlaceIntelligence"));
 const Phase22LegalSettingsEntry = dynamic(() => import("@/components/Phase22LegalSettingsEntry"));
+const Phase25LocationChoiceBridge = dynamic(() => import("@/components/Phase25LocationChoiceBridge"));
 const CommercialSafetyBridge = dynamic(() => import("@/components/CommercialSafetyBridge"));
 const InternalWebsiteBrandBridge = dynamic(() => import("@/components/InternalWebsiteBrandBridge"));
 
 const COMMERCIAL_POLISH_PATHS = new Set(["/promote", "/business", "/search"]);
+const VISIBILITY_SYNC_PATHS = new Set(["/", "/map", "/search", "/following"]);
 
 export default function ProductClientRuntime() {
   const pathname = usePathname();
+  const [detailEnhancementsActive, setDetailEnhancementsActive] = useState(false);
   const isFeed = pathname === "/";
   const isYou = pathname === "/you";
+  const needsVisibilitySync = VISIBILITY_SYNC_PATHS.has(pathname);
   const needsCommercialPolish = COMMERCIAL_POLISH_PATHS.has(pathname);
   const needsInternalBranding = pathname === "/ops" || pathname.startsWith("/moderation");
+
+  useEffect(() => {
+    const activateFromHash = () => {
+      if (window.location.hash.startsWith("#ping=")) setDetailEnhancementsActive(true);
+    };
+    const activateFromDetail = () => setDetailEnhancementsActive(true);
+
+    activateFromHash();
+    window.addEventListener("ping:open-detail", activateFromDetail);
+    window.addEventListener("hashchange", activateFromHash);
+    return () => {
+      window.removeEventListener("ping:open-detail", activateFromDetail);
+      window.removeEventListener("hashchange", activateFromHash);
+    };
+  }, []);
 
   return (
     <>
@@ -43,11 +62,11 @@ export default function ProductClientRuntime() {
       <PrivacySafetyCenter />
       <PasswordAuthOverlay />
       <Phase6NotificationBadge />
-      <Phase7VisibilityBridge />
-      <Phase7ContributorContext />
+      {needsVisibilitySync && <Phase7VisibilityBridge />}
+      {detailEnhancementsActive && <Phase7ContributorContext />}
       {isFeed && <Phase8SinceLastVisit />}
       {isFeed && <Phase8NearbyPulse />}
-      <Phase8FollowBridge />
+      {detailEnhancementsActive && <Phase8FollowBridge />}
       {isFeed && <Phase9PromotedLocal />}
       {isFeed && <Phase14SearchEntry />}
       {isFeed && <Phase15PlaceIntelligence />}
@@ -56,7 +75,7 @@ export default function ProductClientRuntime() {
       <Phase22StorageChoice />
       {isYou && <Phase22LegalSettingsEntry />}
       <Phase24BetaBridge />
-      <Phase25LocationChoiceBridge />
+      {isFeed && <Phase25LocationChoiceBridge />}
       <Phase21AccessibilityBridge />
       {needsCommercialPolish && <CommercialSafetyBridge />}
       <PindrizzleCopyBridge />
